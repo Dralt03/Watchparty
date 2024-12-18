c99ffdc076609fed5c22d47f3d6cb05f572de85c
sauren-sharma <sauren.sharma@zenstreet.ai>
2024-12-18 10:50:05 +0530

    First Commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,101 +1,16 @@
 "use client";
-import React, { useState } from "react";
-import Link from "next/link";
+import SignUpForm from "@/ui/SignUpForm";
+import React from "react";
 
-type SignInFormProps = {
-  onSubmit: (email: string, password: string, username: string) => void;
-};
-
-const SignUpForm: React.FC<SignInFormProps> = ({ onSubmit }) => {
-  const [email, setEmail] = useState<string>("");
-  const [username, setUsername] = useState<string>("");
-
-  const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState<string | null>(null);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (!email || !password) {
-      setError("Email and Password are required.");
-      return;
-    }
-
-    setError(null);
-    onSubmit(email, password, username);
+const page: React.FC = () => {
+  const handleSignUp = (email: string, password: string, username: string) => {
+    console.log(email, password, username);
   };
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-neutral-200">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-xl shadow-md sm:w-96 w-72 min-h-96 flex flex-col justify-between"
-      >
-        <h2 className="text-2xl font-bold text-center mb-4">Sign Up</h2>
-        {error && <p className="text-red-500 text-sm mb-6">{error}</p>}
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Enter your username"
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <Link
-          href="/signin"
-          className="text-sm w-full pl-1/2 text-gray-500 hover:text-gray-700 mb-4"
-        >
-          Already have an account?
-        </Link>
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 transition"
-        >
-          Sign In
-        </button>
-      </form>
+      <SignUpForm onSubmit={handleSignUp} />
     </div>
   );
 };
 
-export default SignUpForm;
+export default page;
